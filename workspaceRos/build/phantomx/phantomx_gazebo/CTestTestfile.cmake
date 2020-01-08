cea73cacfd09231b7ca43f9583d7811e58e4b1c6
Cyril <cyril@controller.access.network>
2020-01-08 12:10:13 +0200

    suppress

--- workspaceRos/build/phantomx/phantomx_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cyril/Documents/uv5.8/Hexabot/workspaceRos/src/phantomx/phantomx_gazebo
-# Build directory: /home/cyril/Documents/uv5.8/Hexabot/workspaceRos/build/phantomx/phantomx_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
